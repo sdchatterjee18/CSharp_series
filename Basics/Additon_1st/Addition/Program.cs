ddb9a9e6dd94fb696f5d878e65adf4402a2159bb
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-10 22:11:57 +0530

    first addition

---
+++ Basics/Additon_1st/Addition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter 2 numbers ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The addition is :{0} ",(a+b));
+            Console.Read();
+        }
+    }
+}
