badd39e0864e1a10613d35741af68606d693615a
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-12 12:20:28 +0530

    new add

---
+++ Basics/user_input/user_input/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter 1st number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("The addition is " + c);
+            Console.Read();
+        }
+    }
+}
