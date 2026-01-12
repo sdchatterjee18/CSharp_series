34bc67bd172e430500ff865e67e769ab7c8fd2b1
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-12 12:27:15 +0530

    NEW ADD

---
+++ Basics/Content_holder/Content_holder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Content_holder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 90;
+            int c = a + b;
+            Console.Write("{0}+{1}={2}",a,b,c);
+            Console.ReadLine();
+        }
+    }
+}
