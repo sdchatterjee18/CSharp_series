2666e9b5e7ed1f42b2e1946c0847b6e195cf6b29
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-13 10:21:00 +0530

    new program add

---
+++ Basics/null_coalescing_operator/null_coalescing_operator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace null_coalescing_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  choose;
+            Console.WriteLine("low for cold/medium for hot/high for very very hot/press enter for nothing");
+            Console.WriteLine("choose what you want");
+            choose = Console.ReadLine();
+            choose = string.IsNullOrEmpty(choose) ? null : choose;
+            choose = choose ?? "Not decided";
+            switch (choose.ToLower())
+            {
+                case "low":
+                    Console.WriteLine("It is cold");
+                    break;
+                case "medium":
+                    Console.WriteLine("It is hot");
+                    break;
+                case "high":
+                    Console.WriteLine("It is very hot");
+                    break;
+                default:
+                    Console.WriteLine(choose);
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
