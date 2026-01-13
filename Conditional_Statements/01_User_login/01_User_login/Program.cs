8521e1e03f16e02144f93bd8a27e00b710ab56f9
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-13 12:56:51 +0530

    New folder (if/else & switch)

---
+++ Conditional_Statements/01_User_login/01_User_login/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_User_login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+            string password = "soumadip@3214";
+            Console.WriteLine("Enter the username");
+            string user = Console.ReadLine();
+            
+            if (user == username)
+            {
+                Console.WriteLine("Enter the password");
+                string pass = Console.ReadLine();
+                if (pass == password)
+                {
+                    Console.WriteLine("Login success");
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("USER NAME DOES NOT MATCH");
+
+            }
+            Console.Read();
+                
+              
+        }
+    }
+}
