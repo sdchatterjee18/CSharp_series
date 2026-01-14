74cc2145ac415fcfef1adb579353bd45ccfff6ea
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-14 18:53:31 +0530

    new program add

---
+++ 02_Operator/05_bitwise_operator/05_bitwise_operator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05_bitwise_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //convert the value into binary and then perform bit operations like | & 
+            int a = 5;//0101 
+    //                       \
+   //                          0001 
+  //                         /
+            int b = 9;//1001
+            int c = (a & b);
+            Console.WriteLine(c);
+            Console.Read();
+        }
+    }
+}
