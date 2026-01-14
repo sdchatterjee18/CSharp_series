919eebb8c9e12bc92399717b19de432cef421a03
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-14 19:19:05 +0530

    add new program

---
+++ 02_Operator/06_ternary_operator/06_ternary_operator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06_ternary_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //short form of if_else 
+            //return 1st expr if condition is true otherwise false
+            Console.WriteLine("Enter A :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter B :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            string check = (a > b) ? "A id big" : ((b > a) ? "B is big" : "both are equal");
+            Console.WriteLine(check);
+            Console.ReadLine();
+        }
+    }
+}
