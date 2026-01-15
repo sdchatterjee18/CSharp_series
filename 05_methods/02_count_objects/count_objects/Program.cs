1f2efba1de356a6b78bbc2eb963d3304887147bf
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-15 20:59:04 +0530

    new program

---
+++ 05_methods/02_count_objects/count_objects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace count_objects
+{
+    class count_obj
+    {
+        public static int c;
+        static count_obj()
+        {
+            c = 0;
+        }
+        public  count_obj()
+        {
+            c++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            count_obj obj1 = new count_obj();
+            count_obj obj2 = new count_obj();
+            count_obj obj3 = new count_obj();
+            Console.Write(count_obj.c);
+            Console.Read();
+        }
+    }
+}
