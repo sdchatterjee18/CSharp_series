54365d6b94e706c8919cd3b3244a585bb7476a42
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-15 21:00:29 +0530

    new program

---
+++ 05_methods/03_Using_params/03_Using_params/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03_Using_params
+{
+    class add
+    {
+        public static void sum(params int[] arr)
+        {
+            int sum = 0;
+            foreach (int n in arr)
+            {
+                sum = sum + n;
+            }
+            Console.WriteLine("The sum is {0}", sum);
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int n;
+            int[] arr = new int[10];
+            Console.WriteLine("Enter how many numbers");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter numbers :");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            add.sum(arr);
+            Console.Read();
+        }
+    }
+}
