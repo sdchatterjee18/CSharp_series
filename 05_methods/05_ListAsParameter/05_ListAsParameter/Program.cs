742c632720773b9a856d5677704c544dad4a4678
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-16 21:24:09 +0530

    new program add

---
+++ 05_methods/05_ListAsParameter/05_ListAsParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_ListAsParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> student =new List<string>();
+            student.Add("souma");
+            student.Add("sonali");
+            student.Add("shreya");
+            List<string> returned_list=names(student);
+            foreach(string name in returned_list)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+
+        static List<string> names(List<string> student)
+        {
+            return student;
+        }
+    }
+}
