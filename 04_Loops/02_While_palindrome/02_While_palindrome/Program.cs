596ae7525c589d99b4692e9369e9df32bea3786a
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-17 20:23:01 +0530

    new porgram add

---
+++ 04_Loops/02_While_palindrome/02_While_palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02_While_palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter any number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            int rev = 0;
+            int temp = n; 
+            while (n > 0)
+            {
+                int rem = n % 10;
+                rev = rev * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("the reverse of {0} is {1}", temp, rev);
+            if (temp == rev)
+            {
+                Console.WriteLine("{0} is plaindrome",temp);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not palindrome",temp);
+            }
+            Console.ReadLine();
+        }
+    }
+}
