6d6c212b74a38914b76ae45d746fb86c951b94d7
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-17 20:17:13 +0530

    new program add

---
+++ 04_Loops/01_While_reverse/01_While_reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_While_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter any number :");
+            n = Convert.ToInt32(Console.ReadLine());
+            int rev = 0;
+            while (n > 0)
+            {
+                int rem = n % 10;
+                rev = rev * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("the reverse of {0} is {1}", n, rev);
+            Console.ReadLine();
+
+        }
+    }
+}
