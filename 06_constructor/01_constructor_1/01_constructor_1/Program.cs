c7bcda797476c2650ca90d5deae9490aab775bec
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-22 22:51:16 +0530

    new program

---
+++ 06_constructor/01_constructor_1/01_constructor_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01_constructor_1
+{
+    class Program
+    {
+        class demo
+        {
+            int x, y;
+            public demo(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public demo()
+            {
+
+            }
+            public void setxy(int x,int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        static void Main(string[] args)
+        {
+            demo d1=new demo();
+            d1.setxy(20, 30);
+            demo d2 = new demo(40, 50);
+            Console.Read();
+        }
+    }
+}
