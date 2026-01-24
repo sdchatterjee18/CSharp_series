ea8299baf786830705d1cf49acdaee84df574a1b
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-24 21:43:39 +0530

    new program

---
+++ 06_constructor/03_Static_constructor/03_objAsParameter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03_objAsParameter
+{
+    class demo
+    {
+        static float pi;
+        int rad;
+        float area1;
+        static demo()//Static constructor is used to initialize static var
+        {                   //allocates memory at compile time before creating any obj
+            pi = 3.14f;
+        }
+        public void area()
+        {
+            Console.WriteLine("Enter the radius :");
+            rad = Convert.ToInt32(Console.ReadLine());
+            area1 = pi * rad * rad;
+            Console.WriteLine("The area of circle is " + area1);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find area of first circle ");
+            demo obj1 = new demo();
+            obj1.area();
+            Console.WriteLine("Find area of second circle ");
+            demo obj2 = new demo();
+            obj2.area();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06_constructor/03_Static_constructor/03_objAsParameter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("03_objAsParameter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("03_objAsParameter")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4c96fa1-8cad-494a-9921-b8c530aa8317")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
