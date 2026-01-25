1eb1b500f865695843ea664e3dea5dd6e4b6308b
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-01-25 20:38:41 +0530

    new program add

---
+++ 06_constructor/04_objAsParameter/04_objAsParameter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04_objAsParameter
+{
+    class student
+    {
+        int id;
+        string name;
+        public student()
+        {
+            Console.WriteLine("Student details");
+        }
+        public void input()
+        {
+            Console.WriteLine("Enter student id :");
+            id=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter student Name :");
+            name =Console.ReadLine(); 
+        }
+        public student(student s1)
+        {
+            s1.input();
+        }
+        public void display()
+        {
+            Console.WriteLine("student id : {0}",id);
+            Console.WriteLine("student Name :{0}",name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            student s2 = new student(s1);
+            s1.display();
+            Console.ReadLine();
+        }
+    }
+}
