e76fa9a9772f8f54282e0de5f728aaeeea32155b
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-02-17 21:08:26 +0530

    Rename

---
+++ 08_Inheritance/01_SingleInheritance/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SBI sbi = new SBI();
+            sbi.bank_name = "State Bank Of India";
+            sbi.branch_name = "Panskura";
+            sbi.address="Panskura R.S.";
+            sbi.gold_loan_percent = 7;
+            sbi.Display_bank_details();
+            sbi.display_goldLoanDetails();
+            Console.Read();
+        }
+    }
+}
