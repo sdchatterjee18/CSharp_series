5ff74cc77628516e0904bb5d0ce3ea6e1f76c2cd
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-02-17 21:05:26 +0530

    New program-single inheritance

---
+++ 08_Inheritance/ConsoleApplication1/ConsoleApplication1/Bank.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Bank
+    {
+        public  string bank_name;
+        public string branch_name;
+        public string address;
+
+        public void Display_bank_details()
+        {
+            Console.WriteLine("----{0}----",bank_name);
+            Console.WriteLine("Branch  Name : {0}", branch_name);
+            Console.WriteLine("Bank address : {0}",address);
+        }
+    }
+}
---
+++ 08_Inheritance/ConsoleApplication1/ConsoleApplication1/SBI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class SBI:Bank
+    {
+        public int gold_loan_percent;
+        public void display_goldLoanDetails()
+        {
+            Console.WriteLine("SBI gold loan at {0}%",gold_loan_percent);
+        }
+    }
+}
