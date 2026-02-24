86751c1eafb98546b2aecdd1e016e042744205f7
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-02-24 13:38:10 +0530

    inheritance program addded

---
+++ 08_Inheritance/02_BaseClassChildClass/01_BaseClassChildClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_BaseClassChildClass
+{
+    class A
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+    class B:A
+    {
+        public override void  display()
+        {
+            Console.WriteLine("chlid class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new B();
+            a1.display();
+            a1 = new A();
+            a1.display();
+            
+            Console.ReadLine();
+        }
+    }
+}
