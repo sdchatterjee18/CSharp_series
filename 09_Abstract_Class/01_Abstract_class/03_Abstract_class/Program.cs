db323589bb975d8def4707b77ad6157d63d00210
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-02-25 00:48:51 +0530

    abstract class new program

---
+++ 09_Abstract_Class/01_Abstract_class/03_Abstract_class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03_Abstract_class
+{
+    abstract class A
+    {
+        public abstract void display();
+        public abstract void show();
+    }
+    abstract class B : A
+    {
+        public override void display()
+        {
+            Console.WriteLine("CLASS B");
+        }
+    }
+    class C:B
+    {
+        //public override void display()
+        //{
+        //    Console.WriteLine("CLASS C");
+        //}
+        public override void show()
+        {
+            Console.WriteLine("CLASS c");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C c = new C();
+            c.display();
+            c.show();
+            Console.Read();
+        }
+    }
+}
