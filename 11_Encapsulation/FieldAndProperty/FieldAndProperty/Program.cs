31a317a5ccbb34e12505e4b2f3a22b30173fa1e1
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-03-14 20:39:23 +0530

    New encapsulation program

---
+++ 11_Encapsulation/FieldAndProperty/FieldAndProperty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FieldAndProperty
+{
+    class Bank
+    {
+        private int balance;
+        public int Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+        public void SetBalance(int a)
+        {
+            balance = a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            //bank.Balance = 200;
+            Console.WriteLine("Enter balance :");
+            int bal = Convert.ToInt32(Console.ReadLine());
+            bank.SetBalance(bal);
+            Console.WriteLine("The balance is :{0}",bank.Balance);
+            Console.ReadLine();
+        }
+    }
+}
