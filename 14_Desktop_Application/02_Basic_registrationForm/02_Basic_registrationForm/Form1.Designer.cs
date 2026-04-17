49cb053fe3bde6e5da76bbe01a795bdd235e667b
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-04-17 23:36:01 +0530

    First Desktop Application - Simple registration page linked with DB

---
+++ 14_Desktop_Application/02_Basic_registrationForm/02_Basic_registrationForm/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace _02_Basic_registrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblconfirmPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lblMassege = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtname.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtname.Location = new System.Drawing.Point(429, 101);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(202, 20);
+            this.txtname.TabIndex = 0;
+            this.txtname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblName.Location = new System.Drawing.Point(317, 101);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Enter Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblState.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblState.Location = new System.Drawing.Point(348, 140);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(32, 13);
+            this.lblState.TabIndex = 3;
+            this.lblState.Text = "State";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(429, 140);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPassword.Location = new System.Drawing.Point(298, 193);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 15);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Enter Password";
+            // 
+            // lblconfirmPassword
+            // 
+            this.lblconfirmPassword.AutoSize = true;
+            this.lblconfirmPassword.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lblconfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblconfirmPassword.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblconfirmPassword.Location = new System.Drawing.Point(287, 238);
+            this.lblconfirmPassword.Name = "lblconfirmPassword";
+            this.lblconfirmPassword.Size = new System.Drawing.Size(93, 15);
+            this.lblconfirmPassword.TabIndex = 6;
+            this.lblconfirmPassword.Text = "Confirm Passowrd";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(429, 193);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(202, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtConfirmPassword.Location = new System.Drawing.Point(429, 238);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(202, 20);
+            this.txtConfirmPassword.TabIndex = 8;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Coral;
+            this.btnRegister.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.Black;
+            this.btnRegister.Location = new System.Drawing.Point(541, 310);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(90, 31);
+            this.btnRegister.TabIndex = 9;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // lblMassege
+            // 
+            this.lblMassege.AutoSize = true;
+            this.lblMassege.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblMassege.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMassege.Location = new System.Drawing.Point(351, 320);
+            this.lblMassege.Name = "lblMassege";
+            this.lblMassege.Size = new System.Drawing.Size(2, 15);
+            this.lblMassege.TabIndex = 10;
+            this.lblMassege.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(293, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(378, 30);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Welcome to Registration page";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(765, 479);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMassege);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblconfirmPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtname);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblconfirmPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label lblMassege;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 14_Desktop_Application/02_Basic_registrationForm/02_Basic_registrationForm/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace _02_Basic_registrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private List<string> GetAllNames()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            List<string> States = null;
+            SqlConnection sqlConnection = null;
+            try
+            {
+                sqlConnection = new SqlConnection(CS);
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("spGetStateNames",sqlConnection))
+                {
+                    sqlDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    DataRowCollection dataRowCollection = dataTable.Rows;
+                    States = new List<string>();
+                    foreach (DataRow row in dataRowCollection)
+                    {
+                        States.Add(row["State_Name"].ToString());
+                    }
+
+                }
+                return States;
+            }
+            catch (Exception ex)
+            {
+                return States;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> States = GetAllNames();
+            if (States != null)
+            {
+                foreach(string state in States)
+                {
+                    comboBox1.Items.Add(state);
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if(txtPassword.Text.Equals(txtConfirmPassword.Text))
+            {
+                int rowseffected=InsertDetailsIntoTable();
+                if(rowseffected!=0)
+                {
+                    lblMassege.Text = "Registration Successfull";
+                    lblMassege.ForeColor = Color.Green;
+                    lblMassege.BackColor = Color.RosyBrown;
+                }
+                else
+                {
+                    lblMassege.Text = "Registration failed !! try again...";
+                    lblMassege.ForeColor = Color.Red;
+                    lblMassege.BackColor = Color.RosyBrown;
+                }
+            }
+            else
+            {
+                lblMassege.Text = "Please match confirm password!!";
+                lblMassege.ForeColor = Color.Red;
+                lblMassege.BackColor = Color.RosyBrown;
+            }
+        }
+        private int InsertDetailsIntoTable()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection sqlConnection = null;
+            int RowsEffected = 0;
+            try
+            {
+                sqlConnection = new SqlConnection(CS);
+                using (SqlCommand sqlCommand = new SqlCommand("spInsertDetailsIntoStudentTable", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@Student_Name", this.txtname.Text);
+                    sqlCommand.Parameters.AddWithValue("@State", this.comboBox1.Text);
+                    sqlCommand.Parameters.AddWithValue("@Password", this.txtPassword.Text);
+                    sqlConnection.Open();
+                    RowsEffected = sqlCommand.ExecuteNonQuery();
+                }
+                return RowsEffected;
+            }
+            catch (Exception ex)
+            {
+                return RowsEffected;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
