c6433f14b9a83aee598b3bd6787eda07e02f657e
Soumadip Chatterjee <soumadipchatterjee18@gmail.com>
2026-04-21 21:21:37 +0530

    Desktop application: multiple forms in onr main panel

---
+++ 14_Desktop_Application/03_Learning_DesignOfWindowsFrom/03_Learning_DesignOfWindowsFrom/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace _03_Learning_DesignOfWindowsFrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelside = new System.Windows.Forms.Panel();
+            this.btn_Employee = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_report = new System.Windows.Forms.Button();
+            this.btn_dashboard = new System.Windows.Forms.Button();
+            this.paneltop = new System.Windows.Forms.Panel();
+            this.mainpanel = new System.Windows.Forms.Panel();
+            this.panelside.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelside
+            // 
+            this.panelside.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelside.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelside.Controls.Add(this.btn_Employee);
+            this.panelside.Controls.Add(this.pictureBox1);
+            this.panelside.Controls.Add(this.btn_report);
+            this.panelside.Controls.Add(this.btn_dashboard);
+            this.panelside.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelside.Location = new System.Drawing.Point(0, 30);
+            this.panelside.Name = "panelside";
+            this.panelside.Size = new System.Drawing.Size(227, 431);
+            this.panelside.TabIndex = 0;
+            // 
+            // btn_Employee
+            // 
+            this.btn_Employee.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Employee.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Employee.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_Employee.Image = ((System.Drawing.Image)(resources.GetObject("btn_Employee.Image")));
+            this.btn_Employee.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Employee.Location = new System.Drawing.Point(3, 225);
+            this.btn_Employee.Name = "btn_Employee";
+            this.btn_Employee.Size = new System.Drawing.Size(221, 56);
+            this.btn_Employee.TabIndex = 0;
+            this.btn_Employee.Text = "EMPLOYEE";
+            this.btn_Employee.UseVisualStyleBackColor = false;
+            this.btn_Employee.Click += new System.EventHandler(this.btn_Employee_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 124);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_report
+            // 
+            this.btn_report.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_report.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_report.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_report.Image = global::_03_Learning_DesignOfWindowsFrom.Properties.Resources.report;
+            this.btn_report.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_report.Location = new System.Drawing.Point(3, 287);
+            this.btn_report.Name = "btn_report";
+            this.btn_report.Size = new System.Drawing.Size(221, 56);
+            this.btn_report.TabIndex = 2;
+            this.btn_report.Text = "REPORT";
+            this.btn_report.UseVisualStyleBackColor = false;
+            this.btn_report.Click += new System.EventHandler(this.btn_report_Click);
+            // 
+            // btn_dashboard
+            // 
+            this.btn_dashboard.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_dashboard.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dashboard.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_dashboard.Image = ((System.Drawing.Image)(resources.GetObject("btn_dashboard.Image")));
+            this.btn_dashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_dashboard.Location = new System.Drawing.Point(3, 163);
+            this.btn_dashboard.Name = "btn_dashboard";
+            this.btn_dashboard.Size = new System.Drawing.Size(221, 56);
+            this.btn_dashboard.TabIndex = 1;
+            this.btn_dashboard.Text = "DASHBOARD";
+            this.btn_dashboard.UseVisualStyleBackColor = false;
+            this.btn_dashboard.Click += new System.EventHandler(this.btn_dashboard_Click);
+            // 
+            // paneltop
+            // 
+            this.paneltop.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.paneltop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.paneltop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paneltop.Location = new System.Drawing.Point(0, 0);
+            this.paneltop.Name = "paneltop";
+            this.paneltop.Size = new System.Drawing.Size(884, 30);
+            this.paneltop.TabIndex = 1;
+            // 
+            // mainpanel
+            // 
+            this.mainpanel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.mainpanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainpanel.Location = new System.Drawing.Point(227, 30);
+            this.mainpanel.Name = "mainpanel";
+            this.mainpanel.Size = new System.Drawing.Size(657, 431);
+            this.mainpanel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.mainpanel);
+            this.Controls.Add(this.panelside);
+            this.Controls.Add(this.paneltop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelside.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelside;
+        private System.Windows.Forms.Panel paneltop;
+        private System.Windows.Forms.Panel mainpanel;
+        private System.Windows.Forms.Button btn_Employee;
+        private System.Windows.Forms.Button btn_report;
+        private System.Windows.Forms.Button btn_dashboard;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+
+
+
+    }
+}
+
---
+++ 14_Desktop_Application/03_Learning_DesignOfWindowsFrom/03_Learning_DesignOfWindowsFrom/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _03_Learning_DesignOfWindowsFrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadFormInPanel(object childForm)
+        {
+            if (this.mainpanel.Controls.Count > 0)
+            {
+                mainpanel.Controls.Clear();
+            }
+            Form f = (Form)childForm;
+            f.TopLevel = false;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.Dock = DockStyle.Fill;
+            mainpanel.Controls.Add(f);
+            mainpanel.Tag = f;
+            f.Show();
+        }
+
+        private void btn_Employee_Click(object sender, EventArgs e)
+        {
+            LoadFormInPanel(new EmployeeForm());
+        }
+
+        private void btn_dashboard_Click(object sender, EventArgs e)
+        {
+            LoadFormInPanel(new dashboard());
+        }
+
+        private void btn_report_Click(object sender, EventArgs e)
+        {
+            LoadFormInPanel(new report());
+        }
+    }
+}
